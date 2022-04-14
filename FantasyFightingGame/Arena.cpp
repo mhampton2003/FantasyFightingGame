f999c61b61b7cd41ac52d02378c695493a7323c7
Maya Hampton <mhampton2003@gmail.com>
2022-04-13 23:22:45 -0700

    TODO

--- FantasyFightingGame/Arena.cpp
+++ FantasyFightingGame/Arena.cpp
@@ -9,3 +9,13 @@ int main() {
 
 	return 0;
 }
+
+/*
+* FIRST ORDER OF BUSINESS
+* load/save character using file IO
+* 
+* 
+* pokemon as inspiration for gameplay?
+* enemy can have selection of attacks contained in array? vector? that is randomly chosen
+* player can choose which potions to use?
+*/
